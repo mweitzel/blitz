5e1515ab237b200c39cafe62e6207289e0276c6c
Matthew Weitzel <mweitzel@gmail.com>
2013-06-28 15:10:44 -0400

    pulled js file out

--- app.rb
+++ app.rb
@@ -29,46 +29,7 @@ __END__
 	<head>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-		<script>
-			preload = {}
-			function goTo(page){ 
-				window.history.pushState({}, "Title", page)
-				loadContent(window.location.pathname)
-			}
-			function loadContent(page){
-				var content = preload[page] || httpOptions(page)
-				document.body.innerHTML = content
-				setTimeout(preloadLinks,0)
-			}
-			function preloadLinks(){
-				$("a").click(function(event){
-					if(!!$(this).attr("preload") && !event.metaKey){
-						event.preventDefault()
-						goTo($(this).attr("href"))
-					}
-				})
-				$("a").each(function(index, link){
-					setTimeout(function(){
-						if(!!link.getAttribute("preload")){
-							var url = link.getAttribute("href")
-							preload[url] = httpOptions(url) 
-						}
-					}, 0)
-				})
-			}
-			window.onpopstate = ps
-			function ps(){
-				console.log(window.location.pathname)
-				setTimeout(function(){loadContent(window.location.pathname)},0)
-			}
-			function httpOptions(theUrl){
-				var xmlHttp = null
-		    xmlHttp = new XMLHttpRequest()
-		    xmlHttp.open( "OPTIONS", theUrl, false )
-		    xmlHttp.send( null )
-		    return xmlHttp.responseText
-			}
-		</script>
+		<script src="blitz.js"></script>
 
 	</head>
 	<body>
